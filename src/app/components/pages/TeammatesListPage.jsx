99cff78a7398ed58b73c27b5e83b62fc77cd2d34
Grigory <rehor.ger@gmail.com>
2022-11-19 17:50:37 +0100

    teammate cards updated

--- src/app/components/pages/TeammatesListPage.jsx
+++ src/app/components/pages/TeammatesListPage.jsx
@@ -7,7 +7,7 @@ const TeammatesListPage = () => {
 	const { isLoading, isSuccess, data } = useReceiveTeammatesQuery({ refetchOnFocus: true })
 
 	return !isLoading && isSuccess ? (
-		<section className='grid grid-cols-3 grid-rows-2 gap-y-10 gap-x-16 justify-center items-baseline w-full h-full p-12'>
+		<section className='grid grid-cols-2 grid-rows-2 gap-y-10 gap-x-16 justify-center items-baseline w-full h-full p-12'>
 			{data.map((teammate) => (
 				<TeammateCard key={teammate.id} data={teammate} />
 			))}
