9fc5e18e8247ccf11fcb7a8d33c908040d4f9bf2
Artemiusz Kuzniecow <arty.kuznetsov@yahoo.com>
2022-11-20 16:16:40 +0300

    fixed a reloading bug

--- src/app/components/ui/teammate/TeammatePageBody.jsx
+++ src/app/components/ui/teammate/TeammatePageBody.jsx
@@ -12,55 +12,59 @@ import PropTypes from 'prop-types'
 
 const TeammatePageBody = ({ data }) => {
 	const navigate = useNavigate()
-	//    max-w-full leading-10 text-center
 	return (
-		<div className='flex flex-col mt-5'>
-			<div className='flex flex-row gap-5 flex-wrap justify-center '>
-				<div className='lg:w-1/2 md:w-1/2 sm:w-full'>
-					<div>
-						<TeammatePic
-							imgClass='block max-w-full mb-11 w-80 rounded-lg'
-							imgPath={data.imgPath}
-						/>
-						<div className='border border-red-700 border-4'>BOOKMARK</div>
-						<BoldText>Задачи на проекте:</BoldText>
-						<UnorderedList>
-							{data.tasks.map((task) => (
-								<ListElement key={task.length}>{task}</ListElement>
-							))}
-						</UnorderedList>
+		data && (
+			<div className='flex flex-col mt-5'>
+				<div className='flex flex-row gap-5 flex-wrap justify-center '>
+					<div className='lg:w-1/2 md:w-1/2 sm:w-full'>
+						<div>
+							<TeammatePic
+								imgClass='block max-w-full mb-11 w-80 rounded-lg'
+								imgPath={data.imgPath}
+							/>
+							<div className='border border-red-700 border-4'>BOOKMARK</div>
+							<BoldText>Задачи на проекте:</BoldText>
+							<UnorderedList>
+								{data.tasks.map((task) => (
+									<ListElement key={task.length}>{task}</ListElement>
+								))}
+							</UnorderedList>
+						</div>
+					</div>
+					<div className='lg:w-1/3 md:w-1/3 sm:w-full'>
+						<div>
+							<TeammatePageInfo infoClass='flex flex-col gap-2 mb-11' data={data} />
+							<TeammatePageInterestsBlock
+								interestsBlockClass='mb-11'
+								hobbiesList={data.hobbies}
+							/>
+							<div>
+								<TeamateProgress
+									HTML={Number(data.technologies.html)}
+									CSS={Number(data.technologies.css)}
+									JScript={Number(data.technologies.js)}
+									React={Number(data.technologies.react)}
+								/>
+							</div>
+						</div>
 					</div>
 				</div>
-				<div className='lg:w-1/3 md:w-1/3 sm:w-full'>
-					<div>
-						<TeammatePageInfo infoClass='flex flex-col gap-2 mb-11' data={data} />
-						<TeammatePageInterestsBlock
-							interestsBlockClass='mb-11'
-							hobbiesList={data.hobbies}
+
+				<div>
+					<div className='border border-red-700 border-4'>PORTFOLIO: </div>
+					<div className='flex justify-around mt-10 flex-wrap'>
+						<TeammatePageContactsList
+							contactsListClass='mb-11'
+							contacts={data.contacts}
 						/>
-						<div>
-							<TeamateProgress
-								HTML={Number(data.technologies.html)}
-								CSS={Number(data.technologies.css)}
-								JScript={Number(data.technologies.js)}
-								React={Number(data.technologies.react)}
-							/>
+						<TeammatePageFeedback feedbackClass='mb-11' />
+						<div className='self-center'>
+							<Button onClick={() => navigate(-1)}>К команде разработки</Button>
 						</div>
 					</div>
 				</div>
 			</div>
-
-			<div>
-				<div className='border border-red-700 border-4'>PORTFOLIO: </div>
-				<div className='flex justify-around mt-10 flex-wrap'>
-					<TeammatePageContactsList contactsListClass='mb-11' contacts={data.contacts} />
-					<TeammatePageFeedback feedbackClass='mb-11' />
-					<div className='self-center'>
-						<Button onClick={() => navigate(-1)}>К команде разработки</Button>
-					</div>
-				</div>
-			</div>
-		</div>
+		)
 	)
 }
 
