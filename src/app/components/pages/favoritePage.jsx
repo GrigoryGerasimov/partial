4a0f913b9488b323e2da98d7ffa1c1a4d430547f
Tai_SaWFly <vawer24@gmail.com>
2022-11-20 12:18:48 +0500

    fix_styles_v1.2.1/short_card_and_actions

--- src/app/components/pages/favoritePage.jsx
+++ src/app/components/pages/favoritePage.jsx
@@ -5,9 +5,11 @@ import Button from '../common/Button'
 import favoriteService from '../../services/favoriteService'
 import ComponentTitle from '../common/componentTitle'
 import ComponentContainer from '../common/componentContainer'
+import { useNavigate } from 'react-router-dom'
 
 const FavoritePage = () => {
 	const { data } = useReceiveTeammatesQuery()
+	const navigate = useNavigate()
 	const [favorite, setFavorite] = useState(favoriteService.getFavorite)
 	const [renderData, setRenderData] = useState()
 
@@ -20,7 +22,7 @@ const FavoritePage = () => {
 		const returnData = []
 		for (const id in dataIdsFromLocal) {
 			for (const d in data) {
-				if (dataIdsFromLocal[id]._id === data[d]._id) {
+				if (dataIdsFromLocal[id]._id === data[d].id) {
 					returnData.push(data[d])
 				}
 			}
@@ -40,12 +42,23 @@ const FavoritePage = () => {
 			<div className='flex flex-wrap justify-around'>
 				{renderData && renderData.length !== 0 ? (
 					renderData.map((t) => (
-						<div key={t._id} className='w-1/3 flex flex-col px-2 justify-around'>
-							<TeammateCard data={t} />
-							<div>
-								<Button onClick={() => handleRemoveFavoriteOnPage(t._id)}>
-									Remove from favorites
-								</Button>
+						<div key={t._id} className='w-1/3 flex px-2 justify-around'>
+							<div className='flex flex-col justify-between max-w-[310px] mb-10 bg-slate-50 transition duration-400 hover:shadow-lg hover:shadow-indigo-200 rounded-xl'>
+								<TeammateCard data={t} />
+
+								<div className='flex justify-around px-2 mb-5'>
+									<div className='w-2/5'>
+										<Button onClick={() => navigate(`/teammates/${t.id}`)}>
+											Открыть
+										</Button>
+									</div>
+
+									<div className='w-2/5'>
+										<Button onClick={() => handleRemoveFavoriteOnPage(t.id)}>
+											Remove
+										</Button>
+									</div>
+								</div>
 							</div>
 						</div>
 					))
--- src/app/components/ui/temmateCard/TeammateCard.jsx
+++ src/app/components/ui/temmateCard/TeammateCard.jsx
@@ -1,34 +1,17 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import TeammatePic from './TeammatePic.jsx'
 import { TeammateInfo } from './TeammateInfo.jsx'
-import Button from '../../common/Button.jsx'
 import PropTypes from 'prop-types'
-import favoriteService from '../../../services/favoriteService'
 
 const TeammateCard = ({ data }) => {
-	const navigate = useNavigate()
-
-	const handleAddFavorite = (id) => {
-		favoriteService.addFavorite(id)
-	}
-
 	return (
-		<section className='flex flex-col justify-between max-w-[310px] mb-10 bg-slate-50 transition duration-400 hover:shadow-lg hover:shadow-indigo-200  rounded-xl relative'>
+		<>
 			<TeammatePic
 				imgClass='w-[100%] h-56 rounded-2xl object-fill'
 				imgPath={data.imgPath}
 			/>
 			<TeammateInfo infoClass='my-4 px-3 text-center' data={data} />
-			<div className='flex justify-around px-2 mb-5'>
-				<div className='w-2/5'>
-					<Button onClick={() => navigate(`/teammates/${data.id}`)}>Открыть</Button>
-				</div>
-				<div className='w-2/5'>
-					<Button onClick={() => handleAddFavorite(data._id)}>Избранное</Button>
-				</div>
-			</div>
-		</section>
+		</>
 	)
 }
 
