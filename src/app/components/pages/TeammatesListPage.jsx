3fc77140edc8d47f4342561e4939248d5152de57
realing29 <realing29@gmail.com>
2022-11-20 22:11:36 +0500

    add loader card:

--- src/app/components/pages/TeammatesListPage.jsx
+++ src/app/components/pages/TeammatesListPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import TeammateCard from '../ui/teammateCard'
-import Loader from '../common/Loader'
 import { useReceiveTeammatesQuery } from '../../store/api.js'
 import ComponentTitle from '../common/Title'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { add } from '../../store/favouriteSlice.js'
 import Button from '../common/Button.jsx'
+import TeammateCardLoader from '../ui/TeammateCardLoader'
 
 const TeammatesListPage = () => {
 	const dispatch = useDispatch()
@@ -19,31 +19,41 @@ const TeammatesListPage = () => {
 		dispatch(add(id))
 	}
 
-	return !isLoading && isSuccess ? (
+	// console.log(!isLoading && isSuccess)
+
+	// const test = 1 - 1
+
+	return (
 		<>
 			<ComponentTitle title='Наша команда' />
 			<div className='flex flex-wrap justify-around'>
-				{data.map((d) => (
-					<div key={d.id} className='w-1/3 flex px-2 justify-around'>
-						<div className='flex flex-col justify-between max-w-[310px] mb-10 bg-slate-50 transition duration-400 hover:shadow-lg hover:shadow-indigo-200 rounded-xl'>
-							<TeammateCard data={d} />
+				{!isLoading && isSuccess
+					? data.map((d) => (
+							<div key={d.id} className='w-1/3 flex px-2 justify-around'>
+								<div className='flex flex-col justify-between max-w-[310px] mb-10 bg-slate-50 transition duration-400 hover:shadow-lg hover:shadow-indigo-200 rounded-xl'>
+									<TeammateCard data={d} />
 
-							<div className='flex justify-around px-2 mb-5'>
-								<div className='w-2/5'>
-									<Button onClick={() => navigate(`/teammates/${d.id}`)}>Открыть</Button>
-								</div>
+									<div className='flex justify-around px-2 mb-5'>
+										<div className='w-2/5'>
+											<Button onClick={() => navigate(`/teammates/${d.id}`)}>
+												Открыть
+											</Button>
+										</div>
 
-								<div className='w-2/5'>
-									<Button onClick={() => handleAddFavourite(d.id)}>Избранное</Button>
+										<div className='w-2/5'>
+											<Button onClick={() => handleAddFavourite(d.id)}>Избранное</Button>
+										</div>
+									</div>
 								</div>
 							</div>
-						</div>
-					</div>
-				))}
+					  ))
+					: [' ', ' ', ' ', ' '].map((item, i) => (
+							<div key={'loaderCard' + i} className='flex px-2 justify-around'>
+								<TeammateCardLoader />
+							</div>
+					  ))}
 			</div>
 		</>
-	) : (
-		<Loader />
 	)
 }
 
