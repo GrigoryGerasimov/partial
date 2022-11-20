80e65236eb977e7405a1df6b1c6d0a9c231b2c3d
Tai_SaWFly <vawer24@gmail.com>
2022-11-21 01:28:25 +0500

    fix_bage

--- src/app/components/common/tables/TableFooter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const TableFooter = ({ children }) => {
-	return <tfoot>{children}</tfoot>
-}
-
-TableFooter.propTypes = {
-	children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
-}
