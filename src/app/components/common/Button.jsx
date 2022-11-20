3930975af817fcc8747733f7fea8def57e5d12e1
realing29 <realing29@gmail.com>
2022-11-20 17:42:15 +0500

    update page teameate

--- src/app/components/common/Button.jsx
+++ src/app/components/common/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button = ({ children, bg = 'bg-indigo-500', ...rest }) => {
+const Button = ({ children, ...rest }) => {
 	return (
 		<button
-			className={`inline-block w-[100%] p-3 ${bg} rounded-md text-slate-50 border-2 border-slate-50 transition duration-400 hover:text-indigo-500 hover:bg-slate-50 hover:border-indigo-500 `}
+			className='inline-block w-[100%] p-3 bg-indigo-500 rounded-md text-slate-50 border-2 border-slate-50 transition duration-400 hover:text-indigo-500 hover:bg-slate-50 hover:border-indigo-500'
 			{...rest}
 		>
 			{children}
@@ -15,6 +15,5 @@ const Button = ({ children, bg = 'bg-indigo-500', ...rest }) => {
 export default Button
 
 Button.propTypes = {
-	bg: PropTypes.string,
-	children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]),
+	children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
 }
