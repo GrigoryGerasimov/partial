b62a5c38340965b75d3f6956810f0331047c78db
realing29 <realing29@gmail.com>
2022-11-20 11:17:04 +0500

    fix bug progress bar

--- src/app/components/ui/temmateCard/TeamateProgress.jsx
+++ src/app/components/ui/temmateCard/TeamateProgress.jsx
@@ -3,22 +3,19 @@ import PropTypes from 'prop-types'
 const TeamateProgress = ({ HTML, CSS, JScript, React }) => {
 	const technology = { HTML, CSS, JScript, React }
 	const colours = {
-		HTML: 'orange-500',
-		CSS: 'blue-500',
-		JScript: 'yellow-500',
-		React: 'blue-300',
+		HTML: 'from-orange-500',
+		CSS: 'from-blue-500',
+		JScript: 'from-yellow-500',
+		React: 'from-blue-300',
 	}
-	console.log(
-		'🚀 ~ file: TeamateProgress.jsx ~ line 11 ~ TeamateProgress ~ colours',
-		colours,
-	)
+
 	return (
 		<div className='flex flex-col max-w-sm h-100 gap-1 bg-stone-800 p-2 text-white rounded-md'>
 			<h2>Знание технологий</h2>
 			{Object.entries(technology).map(([key, value]) => (
 				<div
 					key={'technology' + key}
-					className={`flex place-content-between px-2 bg-gradient-to-l form-${colours[key]} gap-1 rounded-r-lg`}
+					className={`flex place-content-between px-2 bg-gradient-to-l ${colours[key]} gap-1 rounded-r-lg`}
 					style={{ width: `${value}%` }}
 				>
 					<div>{key}</div>
